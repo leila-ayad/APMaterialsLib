cd4b019714d561bb8e1fdebe4fc62f24cb7e41d4
Leila <leila.alhemali@gmail.com>
2022-06-09 19:29:11 +0200

    another old path

--- users/users-router.js
+++ users/users-router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 const User = require("./users-model.js");
-const Material = require("../materials/materials-model")
+const Material = require("../auth/materials/materials-model")
 
 //get a user's active materials
 

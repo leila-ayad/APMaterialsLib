6d4c994d5381a3235fb4b79023ecfe6770d767b4
Leila <leila.alhemali@gmail.com>
2023-02-12 21:46:27 -0700

    switched to Postgres DB and added Heroku db

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'https://ap-materials-lib-fe.vercel.app/',
     'http://127.0.0.1:5500',
     'http://localhost:3000/register',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://localhost:9000'
 ];
 
 module.exports = allowedOrigins;

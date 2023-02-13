2a2881e4111db03d9997aa5109ad975f3abec962
Leila <leila.alhemali@gmail.com>
2023-02-12 22:00:42 -0700

    updated knexfile

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,15 +3,7 @@ const configurations = require('../knexfile.js');
 const environment = process.env.NODE_ENV || 'development';
 
 
-const knex = require('knex')({
-    client: 'pg',
-    connection: {
-      host : 'ec2-3-217-251-77.compute-1.amazonaws.com',
-      user : 'zevipxarlcmcpu',
-      password : 'cecace2b5e44e068a4d5790a2627e6d19f927e0587236172688bdfe7c30166d1',
-      database : 'dfvf3dlr0da74a'
-    }
-  });
+const knex = require('knex')(configurations[environment]);
 
-module.exports = knex(configurations[environment]);
+module.exports = knex; 
     
--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,27 @@
 const sharedConfig = {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    migrations: { directory: './data/migrations' },
-    pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
-  }
-  
-  module.exports = {
-    development: {
-      ...sharedConfig,
-      connection: { filename: './data/database.db3' },
-      seeds: { directory: './data/seeds' },
-    },
-    testing: {
-      ...sharedConfig,
-      connection: { filename: './data/test.db3' },
-    },
-  };
-  
+  client: "pg",
+  connection: {
+    host: "ec2-3-217-251-77.compute-1.amazonaws.com",
+    user: "zevipxarlcmcpu",
+    password:
+      "cecace2b5e44e068a4d5790a2627e6d19f927e0587236172688bdfe7c30166d1",
+    database: "dfvf3dlr0da74a",
+  },
+  useNullAsDefault: true,
+  migrations: { directory: "./data/migrations" },
+  pool: {
+    afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
+  },
+};
+
+module.exports = {
+  development: {
+    ...sharedConfig,
+    connection: { filename: "./data/database.db3" },
+    seeds: { directory: "./data/seeds" },
+  },
+  testing: {
+    ...sharedConfig,
+    connection: { filename: "./data/test.db3" },
+  },
+};

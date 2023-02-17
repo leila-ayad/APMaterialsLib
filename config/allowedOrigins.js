61f0eeaee948831f833ea3fceb418ea32fa01656
Leila <leila.alhemali@gmail.com>
2023-02-17 09:46:07 -0700

    Updated CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
-    'https://ap-materials-lib-fe.vercel.app',
-    'https://ap-materials-lib-fe.vercel.app/',
+    'https://intense-island-04875.herokuapp.com/',
     'http://127.0.0.1:5500',
     'http://localhost:3000/register',
     'http://localhost:3000',

36aa7b195edb06df7bee296ca36aa13a38829b2c
Leila <leila.alhemali@gmail.com>
2023-02-22 13:11:51 -0700

    made it better

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const path = require("path")
-const multer = require("multer")
-
+const path = require("path");
+const multer = require("multer");
 
 const credentials = require("./middleware/credentials");
 
 const cors = require("cors");
-const  corsOptions = require("./config/corsOptions");
+const corsOptions = require("./config/corsOptions");
 
-console.log('here')
+console.log("here");
 
 const usersRouter = require("./users/users-router");
 const materialsRouter = require("./auth/materials/materials-router");
@@ -17,25 +16,27 @@ const authRouter = require("./auth/auth-router.js");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
-
 const server = express();
 
+server.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://intense-island-04875.herokuapp.com"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 server.use(credentials);
 
 server.use(cors(corsOptions));
 
-server.use(function(req, res, next) {
-
-  res.header('Access-Control-Allow-Origin', 'https://intense-island-04875.herokuapp.com');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 //urlencoded for accepting form data
 server.use(bodyParser.urlencoded({ extended: false }));
-
 
 server.use(express.json());
 

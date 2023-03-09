162ec12660a2b7e472897a5e87f59238a0f327b9
Leila <leila.alhemali@gmail.com>
2023-03-09 15:12:07 -0700

    upadted procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./bin/fixie-wrench 8080:ec2-3-229-165-146.compute-1.amazonaws.com:80 & npm run start
+web: ./bin/fixie-wrench 5432:ec2-3-229-165-146.compute-1.amazonaws.com:5432 & npm run start
--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,17 @@
 const development = {
-client: "sqlite3",
-useNullAsDefault: true,
-connection: {
-  filename: "./data/database.db3"
-},
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./data/database.db3",
+  },
   pool: {
     afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
-  }
-}
-
+  },
+};
 
 const production = {
   client: "pg",
-  connection: process.env.DATABASE_URL,
-
+  connection: process.env.DB_URL,
   pool: {
     min: 2,
     max: 10,

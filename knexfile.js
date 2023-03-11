248516e12157c0a12c2699b654e625f35805c12c
Leila <leila.alhemali@gmail.com>
2023-03-11 09:23:30 -0700

    added seed object

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,9 @@ const development = {
   migrations: {
     directory: "./data/migrations"
   },
+  seeds : {
+    directory: "./data/seeds"
+  },
   pool: {
     afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
   },
